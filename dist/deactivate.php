245a2fc19f9e29a4d4201341952dfebc58ead8b8
willphan <phanthanhnha1712000@gmail.com>
2025-01-15 22:08:32 -0500

    Ready for production

--- dist/deactivate.php
+++ dist/deactivate.php
@@ -7,7 +7,7 @@ use config\SystemConfig;
 
 $img = SystemConfig::globalVariables()['img']['deactivate'];
 
-?> <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Document</title><script src="/dist/tailwind6ccb42f1422bd39f0160.js"></script><script src="/dist/universal338525335f3c1d0938f4.js"></script></head><body><div class="logo mt-[20px]"><?= (new Logo())->render(); ?></div><div id="container" class="flex flex-col justify-center items-center"><div class="p-[30px]"><h1 class="text-[20px] shadow-xs rounded-[30px] bg-[#f0f0f0] p-[20px] text-center">Your account is being deactivated, go to sign in to restore your account now</h1></div><div id="code404" class="max-w-[500px]"><img draggable="false" src="<?= $img ?>"></div> <?= (new SigninGoBack())->render(); ?> </div> <?php
+?> <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Document</title><script src="/dist/tailwind638669a4fa4387326ce6.js"></script><script src="/dist/universalcd9b0fe72e36233b9716.js"></script></head><body><div class="logo mt-[20px]"><?= (new Logo())->render(); ?></div><div id="container" class="flex flex-col justify-center items-center"><div class="p-[30px]"><h1 class="text-[20px] shadow-xs rounded-[30px] bg-[#f0f0f0] p-[20px] text-center">Your account is being deactivated, go to sign in to restore your account now</h1></div><div id="code404" class="max-w-[500px]"><img draggable="false" src="<?= $img ?>"></div> <?= (new SigninGoBack())->render(); ?> </div> <?php
     (new Copyright([
         'position' => 'absolute'
     ]))->render();
