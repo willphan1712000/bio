d239be523d47a38f0d553f4d3fb8c867adf9f4b3
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2025-08-04 03:22:06 -0400

    Ready for production - version 7.8.0

--- data/backend/ProductionConfig.php
+++ data/backend/ProductionConfig.php
@@ -31,10 +31,10 @@ enum Bio
 
 class ProductionConfig
 {
-    public static Mode $mode = Mode::DEVELOPMENT; // mode (development or production)
+    public static Mode $mode = Mode::PRODUCTION; // mode (development or production)
     public static Type $type = Type::TEST;
     public static Bio $bio = Bio::PRO;
-    public static $version = "7.7.0"; // version of the product
+    public static $version = "7.8.0"; // version of the product
 
     public static function database()
     {
--- dist/deactivate.php
+++ dist/deactivate.php
@@ -7,7 +7,7 @@ use config\SystemConfig;
 
 $img = SystemConfig::globalVariables()['img']['deactivate'];
 
-?> <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Document</title><script src="/dist/tailwindcb933fd98339b2fb7f03.js"></script><script src="/dist/universala11dc2d6c81a1151b30c.js"></script></head><body><div class="logo mt-[20px]"><?= (new Logo())->render(); ?></div><div id="container" class="flex flex-col justify-center items-center"><div class="p-[30px]"><h1 class="text-[20px] shadow-xs rounded-[30px] bg-[#f0f0f0] p-[20px] text-center">Your account is being deactivated, go to sign in to restore your account now</h1></div><div id="code404" class="max-w-[500px]"><img draggable="false" src="<?= $img ?>"></div> <?= (new SigninGoBack())->render(); ?> </div> <?php
+?> <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Document</title><script src="/dist/tailwind9e3ceb0d80fcfe0e4ccb.js"></script><script src="/dist/universal9fe52e032604f1543a76.js"></script></head><body><div class="logo mt-[20px]"><?= (new Logo())->render(); ?></div><div id="container" class="flex flex-col justify-center items-center"><div class="p-[30px]"><h1 class="text-[20px] shadow-xs rounded-[30px] bg-[#f0f0f0] p-[20px] text-center">Your account is being deactivated, go to sign in to restore your account now</h1></div><div id="code404" class="max-w-[500px]"><img draggable="false" src="<?= $img ?>"></div> <?= (new SigninGoBack())->render(); ?> </div> <?php
     (new Copyright([
         'position' => 'absolute'
     ]))->render();
