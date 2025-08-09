159ff7b1de2ee99043d045055cd5523d0dedba64
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2025-08-09 16:46:56 -0400

    Ready for production - version 7.9.2

--- data/backend/ProductionConfig.php
+++ data/backend/ProductionConfig.php
@@ -31,10 +31,10 @@ enum Bio
 
 class ProductionConfig
 {
-    public static Mode $mode = Mode::DEVELOPMENT; // mode (development or production)
+    public static Mode $mode = Mode::PRODUCTION; // mode (development or production)
     public static Type $type = Type::TEST;
     public static Bio $bio = Bio::PRO;
-    public static $version = "7.9.1"; // version of the product
+    public static $version = "7.9.2"; // version of the product
 
     public static function database()
     {
